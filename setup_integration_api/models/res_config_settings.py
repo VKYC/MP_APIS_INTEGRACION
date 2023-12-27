4dac53b07fb3bbd41eb5d66e8425d69e3868292d
Tonny <corner.store59@gmail.com>
2023-12-26 19:24:28 -0600

    [CHG] Se cambia el nombre en el XML de las APIS

--- setup_integration_api/models/res_config_settings.py
+++ setup_integration_api/models/res_config_settings.py
@@ -6,7 +6,7 @@ from odoo.exceptions import UserError
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    api_first_url = fields.Text(string='Url la url de API 1')
-    api_first_key = fields.Text(string='Url la key de API 1')
-    api_second_url = fields.Text(string='Url la url de API 2')
-    api_second_key = fields.Text(string='Url la key de API 2')
+    api_first_url = fields.Text(string='Url de API Rin de Gastos')
+    api_first_key = fields.Text(string='Key de API Rin de Gastos')
+    api_second_url = fields.Text(string='Url de API SII')
+    api_second_key = fields.Text(string='Key de API SII')
